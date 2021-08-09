db93da3926699fc1a16d488dc55265429924d448
Shraddha Gaikwad <66875937+shraddha-pg@users.noreply.github.com>
2021-08-09 14:29:05 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,354 @@
+const Indian = [
+  {
+    id: 1,
+    title: "Chhole Bhature",
+    category: "india",
+    img:
+      "https://media.vogue.in/wp-content/uploads/2020/08/chole-bhature-recipe.jpg",
+    desc: `Chole bhature, also known as ‘channa bhatura’ is a North-Indian must-have. A combination of two dishes—chole, which is a spicy chickpea curry, and bhature, a type of fried bread made with maida—it makes for a meal that’s just right for Sunday afternoons, and should ideally be followed by a post-lunch snooze.`,
+    URL: `https://www.vogue.in/vogue-cookbook/collection/punjabi-style-chole-bhature-recipe-to-make-at-home/`
+  },
+  {
+    id: 2,
+    title: "Bharwa Bhindi",
+    category: "india",
+    img:
+      "https://www.cookwithmanali.com/wp-content/uploads/2018/04/Bharwa-Bhindi.jpg",
+    desc: `Bharwa Bhindi is a popular Indian side dish where the okra (bhindi) is stuffed with a spicy filling. This vegan dish goes really well with flatbread like roti or paratha.`,
+    URL: `https://www.cookwithmanali.com/bharwa-bhindi/`
+  },
+  {
+    id: 3,
+    title: "Pindi Chana",
+    category: "india",
+    img:
+      "https://www.indianfoodforever.com/iffwd/wp-content/uploads/pindi-chole-no-onion-garlic.jpg?x12379",
+    desc: `This chana pindi recipe is something every Punjabi food lover should try at least once. Spicy, filling and delicious, this recipe can be prepared in under an hour. If you are a fan of North Indian cuisine, this pindi chole recipe will become your favourite.`,
+    URL: `https://www.archanaskitchen.com/chole-pindi-recipe-spicy-chickpea-curry`
+  },
+  {
+    id: 4,
+    title: "Jeera Aloo",
+    category: "india",
+    img:
+      "https://www.cubesnjuliennes.com/wp-content/uploads/2021/02/Jeera-Aloo-1.jpg",
+    desc: `Jeera Aloo is a delicious vegetarian Indian dhaba style side dish recipe for boiled potatoes cooked in cumin seeds. A flavorful potato dish which you can prepare just in few minutes with only handful of ingredients. `,
+    URL: `https://www.cubesnjuliennes.com/aloo-jeera-aloo-recipe/`
+  },
+  {
+    id: 5,
+    title: "Kulche",
+    category: "india",
+    img:
+      "https://1.bp.blogspot.com/-z0mlXh3oM2k/X1rUrA9WZTI/AAAAAAAAR4Y/C2nsztIcWXcQUtBdlCfQVpcNzl_uSxAmQCLcBGAsYHQ/s1000/Chole%2BKulche.webp",
+    desc: `One of the best Indian snacks cum breakfast options out there is Chole kulche! Originated from the Punjab region, this dish is loved all over the country and is now making its way in the west too. A delicious combination of an all-purpose flour bread tossed in butter along with tangy and spicy chole, chole kulche is one such dish that we Indians just cannot resist. `,
+    URL: `https://www.vegrecipesofindia.com/kulcha/`
+  },
+  {
+    id: 6,
+    title: "Aloo Parantha",
+    category: "india",
+    img:
+      "https://cdn.cdnparenting.com/articles/2020/01/19152919/Aloo-Paratha-Recipe.webp",
+    desc: `Aloo Paratha is a bread dish originating from the Indian subcontinent. It is a breakfast dish originated from the Punjab region. The recipe is one of the most popular breakfast dishes throughout the western, central and northern regions of India as well as the eastern regions of Pakistan.`,
+    URL: `https://hebbarskitchen.com/aloo-paratha-recipe-aalu-ka-paratha-aloo/`
+  },
+  {
+    id: 7,
+    title: "Biryani",
+    category: "india",
+    img:
+      "https://www.licious.in/blog/wp-content/uploads/2020/12/Hyderabadi-chicken-Biryani.jpg",
+    desc: `Biryani is a mixed rice dish originating among the Muslims of the Indian subcontinent. It is made with Indian spices, rice, and meat usually that of chicken, goat, lamb, prawn, fish, and sometimes, in addition, eggs or vegetables such as potatoes in certain regional varieties.`,
+    URL: `https://www.indianhealthyrecipes.com/chicken-biryani-recipe/`
+  },
+  {
+    id: 8,
+    title: "Idli",
+    category: "india",
+    img: "https://spicecravings.com/wp-content/uploads/2020/06/Idli-2-1.jpg",
+    desc: `Idli or idly are a type of savoury rice cake, originating from the Indian subcontinent, popular as breakfast foods in Southern India and in Sri Lanka. The cakes are made by steaming a batter consisting of fermented black lentils and rice. `,
+    URL: `https://hebbarskitchen.com/idli-recipe-soft-idli-idli-rava/`
+  },
+
+  {
+    id: 9,
+    title: "Aloo gobi",
+    category: "india",
+    img:
+      "https://cdn.shortpixel.ai/spai/w_780+q_lossy+ret_img+to_webp/https://veganbell.com/wp-content/uploads/2020/08/Aloo-Gobi-6.jpg",
+    desc: `Aloo gobi is a vegetarian dish from the Indian subcontinent made with potatoes, cauliflower, and Indian spices. It is popular in Indian and Pakistani cuisines. It is yellowish in colour due to the use of turmeric, and occasionally contains kalonji and curry leaves.`,
+    URL: `https://hebbarskitchen.com/aloo-gobi-dry-recipe-aloo-gobhi-ki-sabji/`
+  },
+  {
+    id: 10,
+    title: "Butter chicken",
+    category: "india",
+    img:
+      "https://www.licious.in/blog/wp-content/uploads/2020/10/butter-chicken-.jpg",
+    desc: `Butter chicken or murgh makhani is a curry of chicken in a spiced tomato, butter and cream sauce. It originated in the Indian subcontinent as a curry. It is similar to chicken tikka masala, which uses a tomato paste.`,
+    URL: `https://www.indianhealthyrecipes.com/butter-chicken/`
+  },
+  {
+    id: 11,
+    title: "Chana masala",
+    category: "india",
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRNfJ4Q0b-1_cleO82vWQowzPdC3x_YaJbqA&usqp=CAU",
+    desc: `Chana masala literally 'mix-spiced small-chickpeas'), also known as channay, chole masala, chhole masala, chole or chholay (plural), is a dish originating from the Indian subcontinent.`,
+    URL: `https://hebbarskitchen.com/chana-masala-recipe-chickpea-masala/`
+  },
+  {
+    id: 12,
+    title: "Fish Curry",
+    category: "india",
+    img:
+      "https://www.licious.in/blog/wp-content/uploads/2020/12/Perfectly-balanced-fish-curry.jpg",
+    desc: `Malabar matthi curry, also known as fish curry, is an Indian dish. It consists of sardines semi-stewed in a Kerala-style curry with assorted vegetables, such as okra or onions. It is usually served with rice, naan, bread, or tapioca.`,
+    URL: `https://www.licious.in/blog/recipe/fish-curry-recipe-using-basic-ingredients/`
+  },
+  {
+    id: 13,
+    title: "Kofta",
+    category: "india",
+    img:
+      "https://www.chilitochoc.com/wp-content/uploads/2020/03/koftacurry.jpg",
+    desc: `Kofta is a family of meatball or meatloaf dishes found in the Indian subcontinent, South Caucasian, Middle Eastern, Balkan, and Central Asian cuisines. In the simplest form, koftas consist of balls of ground meat - usually beef, chicken, lamb or mutton, or a mixture - mixed with spices or onions.`,
+    URL: `https://hebbarskitchen.com/malai-kofta-recipe-malai-kofta-curry/`
+  }, 
+];
+const Italian = [
+  {
+    id: 1,
+    title: "Pizza Napoletana (Naples)",
+    category: "itly",
+    img:
+      "https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg",
+    desc: `Neapolitan pizza (Italian: pizza napoletana) also known as Naples-style pizza, is a style of pizza made with tomatoes and mozzarella cheese. It must be made with either San Marzano tomatoes or Pomodorino del Piennolo del Vesuvio, which grow on the volcanic plains to the south of Mount Vesuvius, and Mozzarella di Bufala Campana, a protected designation of origin cheese made with the milk from water buffalo raised in the marshlands of Campania and Lazio in a semi-wild state, or "Mozzarella STG", a cow's milk mozzarella.`,
+    URL: `https://www.tasteatlas.com/pizza-napoletana/recipe`
+  },
+  {
+    id: 2,
+    title: "Lasagna (Bologna)",
+    category: "itly",
+    img:
+      "https://upload.wikimedia.org/wikipedia/commons/b/ba/Lasagne_-_stonesoup.jpg",
+    desc: `Lasagne, or the singular lasagna, is an Italian dish made of stacked layers of thin flat pasta alternating with fillings such as ragù (ground meats and tomato sauce) and other vegetables, cheese (which may include ricotta and parmesan), and seasonings and spices such as garlic, oregano and basil.[2] The dish may be topped with melted grated mozzarella cheese. `,
+    URL: `https://www.simplyrecipes.com/recipes/lasagna_bolognese/`
+  },
+  {
+    id: 3,
+    title: "Gelato (all over Italy)",
+    category: "itly",
+    img: "https://upload.wikimedia.org/wikipedia/commons/4/47/CafeMia.jpg",
+    desc: `Gelato is a frozen dessert of Italian origin. It is made with a base of 3.25% butterfat whole milk and sugar. It is generally lower in fat than other styles of frozen desserts.`,
+    URL: `https://www.thespruceeats.com/authentic-gelato-recipe-4172134`
+  },
+  {
+    id: 4,
+    title: "Ossobuco alla Milanese (Milan)",
+    category: "itly",
+    img:
+      "https://cdn.tasteatlas.com/images/recipes/25db9be7d59b448882e9f23f92478bca.jpg?mw=1300",
+    desc: `Ossobuco or osso buco is a specialty of Lombard cuisine of cross-cut veal shanks braised with vegetables, white wine and broth. It is often garnished with gremolata and traditionally served with either risotto alla milanese or polenta, depending on the regional variation.The marrow in the hole in the bone, a prized delicacy, is the defining feature of the dish`,
+    URL: `https://www.greatitalianchefs.com/recipes/ossobuco-alla-milanese-recipe`
+  },
+  {
+    id: 5,
+    title: "Panzanella (Tuscany)",
+    category: "itly",
+    img:
+      "https://i2.wp.com/italianfoodfast.com/wp-content/uploads/2020/05/Panzanella-Toscany-scaled.jpg?resize=1170%2C617&ssl=1",
+    desc: `Panzanella or panmolle is a Tuscan chopped salad of soaked stale bread, onions and tomatoes that is popular in the summer. It often includes cucumbers, sometimes basil and is dressed with olive oil and vinegar. It is also popular in other parts of central Italy.`,
+    URL: `https://www.seriouseats.com/classic-panzanella-salad-recipe`
+  },
+  {
+    id: 6,
+    title: "Focaccia (Liguria)",
+    category: "itly",
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvGTuU3Ru2U3FYtzIQXhlII7qbmrzEtDO6d8EePpG4YioOYKwVTUATnv6kFXJX543MMik&usqp=CAU",
+    desc: `Focaccia is a flat oven-baked Italian bread similar in style and texture to pizza; in some places, it is called "pizza bianca". Focaccia can be served as a side dish or as sandwich bread.`,
+    URL: `https://www.asmallkitcheningenoa.com/traditional-ligurian-focaccia-indirect-method/`
+  },
+  {
+    id: 7,
+    title: "Spaghetti alla Carbonara",
+    category: "itly",
+    img:
+      "https://upload.wikimedia.org/wikipedia/commons/3/33/Espaguetis_carbonara.jpg",
+    desc: `Carbonara is an Italian pasta dish from Rome made with egg, hard cheese, cured pork, and black pepper.Spaghetti is the most common pasta, but fettuccine, rigatoni, linguine, or bucatini are also used. Normally guanciale or pancetta are used for the meat component, but lardons of smoked bacon are a common substitute outside Italy.`,
+    URL: `https://www.recipesfromitaly.com/spaghetti-carbonara-original-recipe/`
+  },
+  {
+    id: 8,
+    title: "Cicchetti",
+    category: "itly",
+    img:
+      "https://mirandalovestravelling.com/media/2016/08/night-11-768x508.jpg",
+    desc: `Cicchetti include tiny sandwiches, plates of olives or other vegetables, halved hard boiled eggs, small servings of a combination of one or more of seafood, meat and vegetable ingredients laid on top of a slice of bread or polenta,and very small servings of typical full-course plates. Like Spanish tapas, one can also make a meal of cicchetti by ordering multiple plates.`,
+    URL: `https://www.sanpellegrinofruitbeverages.com/au/citrus/italian-food-recipes/venetian-cicchetti`
+  },
+  {
+    id: 9,
+    title: "Caponata (Sicily)",
+    category: "itly",
+    img:
+      "https://www.themediterraneandish.com/wp-content/uploads/2020/07/caponata-recipe-11.jpg",
+    desc: `Caponata is a Sicilian dish consisting of chopped fried aubergine and other vegetables, seasoned with olive oil, tomato sauce, celery, olives, and capers, in an agrodolce sauce. Numerous local variants exist concerning the ingredients, by adding carrots, bell peppers, potatoes, pine nuts, and raisins.`,
+    URL: `https://cookieandkate.com/caponata-recipe/`
+  },
+  {
+    id: 10,
+    title: "Burrata (Puglia)",
+    category: "itly",
+    img: "https://upload.wikimedia.org/wikipedia/commons/f/f1/Burrata2.jpg",
+    desc: `Burrata is an Italian cow milk cheese (occasionally buffalo milk) made from mozzarella and cream.The outer casing is solid cheese, while the inside contains stracciatella and cream, giving it an unusual, soft texture. It is typical of Apulia.`,
+    URL: `https://gluttodigest.com/burrata-an-italian-cheese-that-literally-bursts-with-goodness/`
+  },
+];
+const Japanese = [
+  {
+    id: 24,
+    title: "Teishoku",
+    category: "japan",
+    img:
+      "https://resources.matcha-jp.com/resize/720x2000/2020/05/15-102865.jpeg",
+    desc: `In Japan, a “meal set” refers generally to combination of white rice, miso soup, “sozai,” (which can be any of variety of possible side dishes) and picked vegetables (called “Tsukemono”) which come with the main item ordered. Many different food items are offered as Teishoku. The most common types of food offered as Teishoku are grilled items, deep fried items, broiled items, noodle-centric items and Chinese dishes.`,
+    URL: `https://iamafoodblog.com/teishoku-breakfast-how-to-make-a-traditional-japanese-breakfast-set/`
+  },
+  {
+    id: 25,
+    title: "Tempura",
+    category: "japan",
+    img:
+      "https://www.thespruceeats.com/thmb/uLxxn8uBZXG0gd5KxtUiUoEpAHo=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/basic-tempura-for-fish-and-seafood-1300531-hero-01-bbd91b4ee23940749fec5ab7693eadcc.jpg",
+    desc: `Tempura is a typical Japanese dish usually consisting of seafood, meat, and vegetables that have been battered and deep fried.`,
+    URL: `https://www.thespruceeats.com/basic-tempura-for-fish-and-seafood-1300531`
+  },
+  {
+    id: 26,
+    title: "Okonomiyaki",
+    category: "japan",
+    img:
+      "https://www.sbs.com.au/food/sites/sbs.com.au.food/files/styles/full/public/asia-unplated_ep03_okonomiyaki_mg_5646.jpg?itok=BVFTCtVQ&mtime=1627450438",
+    desc: `Okonomiyaki is a Japanese savory pancake consisting of various ingredients mixed into a wheat flour batter. Common additions include meat, seafood, and cabbage, and popular toppings include okonomiyaki sauce, aonori, katsuobushi, Japanese mayonnaise, and pickled ginger.`,
+    URL: `https://www.loveandlemons.com/okonomiyaki/`
+  },
+];
+const American = [
+  {
+    id: 27,
+    title: "Cheeseburger",
+    category: "america",
+    img:
+      "https://www.sargento.com/assets/Uploads/Recipe/Image/burger_0__FillWzExNzAsNTgzXQ.jpg",
+    desc: `A cheeseburger is a hamburger topped with cheese. Traditionally, the slice of cheese is placed on top of the meat patty. The cheese is usually added to the cooking hamburger patty shortly before serving, which allows the cheese to melt. Cheeseburgers can include variations in structure, ingredients and composition.`,
+    URL: `https://www.foodrepublic.com/recipes/all-american-cheeseburger-recipe/`
+  },
+  {
+    id: 28,
+    title: "Reuben sandwich",
+    category: "america",
+    img:
+      "https://www.simplyrecipes.com/thmb/mseE_I9vgKZXGVfHQWXBZRxWa7w=/2041x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Reuben-Sandwich-Lead-3-761dccc3acc84e4fb982847063708c55.jpg",
+    desc: `The Reuben sandwich is a North American grilled sandwich composed of corned beef, Swiss cheese, sauerkraut, and Russian dressing, grilled between slices of rye bread. It is associated with kosher-style delicatessens, but it is not kosher, because it combines meat and cheese. `,
+    URL: `https://recipes.timesofindia.com/recipes/reuben-sandwich/rs57752502.cms`
+  },
+  {
+    id: 29,
+    title: "Hot dogs",
+    category: "america",
+    img:
+      "https://i2.wp.com/www.eatthis.com/wp-content/uploads/2019/06/hot-dog-combinations-with-crazy-toppings.jpg?resize=640%2C360&ssl=1",
+    desc: `A hot dog is a dish consisting of a grilled or steamed sausage served in the slit of a partially sliced bun. The term hot dog can also refer to the sausage itself. The sausage used is a wiener or a frankfurter. The names of these sausages also commonly refer to their assembled dish.`,
+    URL: `https://hebbarskitchen.com/2-ways-desi-veg-hot-dog-recipes/`
+  },
+  {
+    id: 30,
+    title: "Philly cheese steak",
+    category: "america",
+    img:
+      "https://thestayathomechef.com/wp-content/uploads/2020/09/Authentic-Philly-Cheesesteak-5.jpg",
+    desc: `A cheesesteak is a sandwich made from thinly sliced pieces of beefsteak and melted cheese in a long hoagie roll. A popular regional fast food, it has its roots in the U.S. city of Philadelphia, Pennsylvania.`,
+    URL: `https://natashaskitchen.com/philly-cheesesteak/`
+  },
+  {
+    id: 31,
+    title: "Nachos",
+    category: "america",
+    img:
+      "https://upload.wikimedia.org/wikipedia/commons/8/87/Nachos-cheese.jpg",
+    desc: `Nachos are a Mexican regional dish from northern Mexico that consists of heated tortilla chips or totopos covered with melted cheese, often served as a snack or appetizer. More elaborate versions of the dish include other ingredients, and may be substantial enough to serve as a main dish.`,
+    URL: `https://www.simplyrecipes.com/recipes/nachos/`
+  }
+];
+
+const img = document.querySelector('.photo');
+const title = document.getElementById('title');
+const desc = document.querySelector('.item-text');
+const link = document.querySelector('.recipe');
+const select = document.getElementById('cuisine');
+let currentItem = 0;
+let userselection = ""
+
+const IN = document.getElementById("india");
+const IT = document.getElementById("itly");
+const JP = document.getElementById("japan");
+const US = document.getElementById("america");
+
+window.addEventListener("DOMContentLoaded", function(){
+  let choice = Indian[currentItem];
+  img.src = choice.img;
+  title.textContent = choice.title;
+  desc.textContent = choice.desc;
+  link.textContent = `Recipe`;
+  link.href = choice.URL;
+})
+
+IN.addEventListener("click", function () {
+  userselection = IN.textContent;
+  select.textContent = " ";
+  dish(userselection);
+});
+
+IT.addEventListener("click", function () {
+  userselection = IT.textContent;
+  select.textContent = " ";
+  dish(userselection);
+});
+
+JP.addEventListener("click", function () {
+  userselection = JP.textContent;
+  select.textContent = " ";
+  dish(userselection);
+});
+
+US.addEventListener("click", function () {
+  userselection = US.textContent;
+  select.textContent = " ";
+  dish(userselection);
+});
+
+function dish(select){
+  let choice = null;
+if(select === 'Indian'){
+ let currentItem = Math.floor(Math.random() * Indian.length);
+ choice = Indian[currentItem]; 
+}else if(select === 'Japanese'){
+ let currentItem = Math.floor(Math.random() * Japanese.length);
+ choice = Japanese[currentItem]; 
+}else if(select === 'Italian'){
+ let currentItem = Math.floor(Math.random() * Italian.length);
+  choice = Italian[currentItem]; 
+}else if (select === 'American'){
+ let currentItem = Math.floor(Math.random() * American.length);
+  choice = American[currentItem]; 
+}
+  img.src = choice.img;
+  title.textContent = choice.title;
+  desc.textContent = choice.desc;
+  link.textContent = `Recipe`;
+  link.href = choice.URL;
+}
+
